a8a55f03e76667f9d4182f6467d733d9a1890b32
Jake Neal <hello@jakeneal.co.uk>
2024-08-13 15:16:53 +0100

    🔧 Added spacing and breakpoints

--- packages/mui-theme/index.ts
+++ packages/mui-theme/index.ts
@@ -7,6 +7,32 @@ const nivodaTheme = createTheme({
             main: tokens.ColorsNeutralBlack,
         },
     },
+    spacing: [
+        tokens.Spacing1,
+        tokens.Spacing2,
+        tokens.Spacing3,
+        tokens.Spacing4,
+        tokens.Spacing5,
+        tokens.Spacing6,
+        tokens.Spacing7,
+        tokens.Spacing8,
+        tokens.Spacing9,
+        tokens.Spacing10,
+        tokens.Spacing12,
+        tokens.Spacing14,
+        tokens.Spacing16,
+        tokens.Spacing18,
+        tokens.Spacing20,
+    ],
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1200,
+            xl: 1536,
+        },
+    },
 });
 
 export default nivodaTheme;
