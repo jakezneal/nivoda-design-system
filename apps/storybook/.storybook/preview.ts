330b46d486bc3a92f415e72ba63fce7612efb0aa
Jake Neal <hello@jakeneal.co.uk>
2024-08-13 17:00:12 +0100

    :fire: Tidied up Storybook

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,14 +1,35 @@
 import type { Preview } from '@storybook/react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import theme from '@nivoda/mui-theme';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
 
 const preview: Preview = {
     parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
         controls: {
+            expanded: true,
             matchers: {
                 color: /(background|color)$/i,
                 date: /Date$/i,
             },
         },
     },
+    decorators: [
+        withThemeFromJSXProvider({
+            themes: {
+                light: theme,
+            },
+            defaultTheme: 'light',
+            Provider: ThemeProvider,
+            GlobalStyles: CssBaseline,
+        }),
+    ],
 };
 
 export default preview;
