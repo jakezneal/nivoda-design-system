bb6b5b55833369b39003190d923a9c21b1dee771
Jake Neal <hello@jakeneal.co.uk>
2024-08-15 10:25:33 +0100

    :wrench: Tidy up Storybook config

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -13,32 +13,15 @@ function getAbsolutePath(value: string): any {
 const config: StorybookConfig = {
     stories: ['../../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
     addons: [
-        getAbsolutePath('@storybook/addon-onboarding'),
         getAbsolutePath('@storybook/addon-links'),
         getAbsolutePath('@storybook/addon-essentials'),
         getAbsolutePath('@chromatic-com/storybook'),
         getAbsolutePath('@storybook/addon-interactions'),
-        '@storybook/addon-themes'
+        getAbsolutePath('@storybook/addon-themes'),
     ],
     framework: {
         name: getAbsolutePath('@storybook/react-vite'),
         options: {},
-    },
-    typescript: {
-        reactDocgen: 'react-docgen-typescript',
-        reactDocgenTypescriptOptions: {
-            // Speeds up Storybook build time
-            compilerOptions: {
-                allowSyntheticDefaultImports: false,
-                esModuleInterop: false,
-            },
-            // Makes union prop types like variant and size appear as select controls
-            shouldExtractLiteralValuesFromEnum: true,
-            // Makes string and boolean types that can be undefined appear as inputs and switches
-            shouldRemoveUndefinedFromOptional: true,
-            // Filter out third-party props from node_modules except @mui packages
-            propFilter: (prop) => (prop.parent ? !/node_modules\/(?!@mui)/.test(prop.parent.fileName) : true),
-        },
     },
 };
 
