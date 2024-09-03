a19c68763ada61f51ea3dba8fee3b81d8dfd3db6
Jake Neal <hello@jakeneal.co.uk>
2024-09-03 12:55:25 +0100

    :sparkles: Added ability to show/hide icon and choose icon

--- packages/ui/src/components/feedback/Alert/Alert.stories.tsx
+++ packages/ui/src/components/feedback/Alert/Alert.stories.tsx
@@ -1,27 +1,71 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { IconAlertTriangle, IconInfoCircle, IconCircleCheck, IconHelpCircle } from '@tabler/icons-react';
 
-import { Alert } from './Alert';
+import { Alert, AlertProps } from './Alert';
 
-const meta: Meta<typeof Alert> = {
+interface AlertPropsAndCustomArgs extends AlertProps {
+    showIcon?: boolean;
+    chosenIcon?: string;
+}
+
+const meta: Meta<AlertPropsAndCustomArgs> = {
     component: Alert,
     argTypes: {
         severity: {
             options: ['success', 'info', 'warning', 'error'],
             control: { type: 'select' },
         },
+        showIcon: {
+            control: { type: 'boolean' },
+        },
+        chosenIcon: {
+            control: { type: 'select' },
+            options: ['alert-triange', 'info-circle', 'circle-check', 'help-circle'],
+            description: 'Takes a Tabler icon',
+        },
     },
     parameters: {
         layout: 'centered',
     },
-    render: (args) => <Alert {...args}>Alert body should go here.</Alert>,
+    render: (args) => {
+        const { showIcon, chosenIcon } = args;
+
+        const AlertIcon = ({ icon }: { icon: string }) => {
+            let iconToShow: React.ReactNode = <IconAlertTriangle />;
+
+            switch (icon) {
+                case 'info-circle':
+                    iconToShow = <IconInfoCircle />;
+                    break;
+                case 'circle-check':
+                    iconToShow = <IconCircleCheck />;
+                    break;
+                case 'help-circle':
+                    iconToShow = <IconHelpCircle />;
+                    break;
+                default:
+                    iconToShow = <IconAlertTriangle />;
+                    break;
+            }
+
+            return iconToShow;
+        };
+
+        return (
+            <Alert icon={showIcon ? <AlertIcon icon={chosenIcon ?? 'alert-triangle'} /> : false} {...args}>
+                Alert body should go here.
+            </Alert>
+        );
+    },
 };
 
-type Story = StoryObj<typeof Alert>;
+type Story = StoryObj<AlertPropsAndCustomArgs>;
 
 export const alert: Story = {
     args: {
         severity: 'success',
         title: '',
+        showIcon: false,
     },
 };
 
