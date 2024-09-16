ccfa7676db1d142160ea32100bddc136febb009a
Jake Neal <hello@jakeneal.co.uk>
2024-09-16 23:17:40 +0100

    :bug: Adjusted icon sizing

--- packages/mui-theme/src/components/navigation/Link.ts
+++ packages/mui-theme/src/components/navigation/Link.ts
@@ -5,11 +5,17 @@ import tokens from '@nivoda/design-tokens';
 const MuiLinkStyles = {
     styleOverrides: {
         root: {
+            alignItems: 'center',
             color: tokens.components.link.text.default,
             display: 'inline-flex',
             gap: tokens.components.link.spacing.gap,
             textDecorationColor: tokens.components.link.underline.default,
             textUnderlineOffset: '3px',
+
+            svg: {
+                width: '1.25em',
+                height: '1.25em',
+            },
 
             ':hover, :focus': {
                 color: tokens.components.link.text.hover,
