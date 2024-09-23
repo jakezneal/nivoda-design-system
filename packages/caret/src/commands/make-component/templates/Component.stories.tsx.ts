7730f4c03f610f60e083242e3108364547d1b6aa
Jake Neal <hello@jakeneal.co.uk>
2024-09-23 09:53:49 +0100

    :art: Adjust Story template

--- packages/caret/src/commands/make-component/templates/Component.stories.tsx.ts
+++ packages/caret/src/commands/make-component/templates/Component.stories.tsx.ts
@@ -6,14 +6,20 @@ import { ${value.component.name.pascal}, type ${value.component.name.pascal}Prop
 
 interface ${value.component.name.pascal}PropsAndCustomArgs extends ${value.component.name.pascal}Props {}
 
-const meta: Meta<${value.component.name.pascal}PropsAndCustomArgs> = {
+const meta = {
     component: ${value.component.name.pascal},
+    argTypes: {},
     parameters: {
         layout: 'centered',
     },
-};
+    render: (args) => {
+        return <${value.component.name.pascal} 
+            {...args} 
+        />
+    },
+} satisfies Meta<${value.component.name.pascal}PropsAndCustomArgs>;
 
-type Story = StoryObj<typeof ${value.component.name.pascal}>;
+type Story = StoryObj<typeof meta>;
 
 export const ${value.component.name.camel}: Story = {
     args: {},
